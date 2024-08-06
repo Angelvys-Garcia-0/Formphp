d7f4f76ca514d2c1e81e31d2ad6f028817d1e0e4
Angelvys-Garcia-0 <angelvysagd@gmail.com>
2024-08-06 14:55:43 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,10 +1,10 @@
 <?
 $Identificacion= $_POST ['Identificacion'];
-$NomApellido= $_POST ['NomApellido'];
+$NomApellidos= $_POST ['NomApellidos'];
 $Cargo= $_POST ['Cargo'];
 $Salario= $_POST ['Salario'];
 echo "LA IDENTIFICACION DEL EMPLEADO ES:   ".Identificacion;
-echo "LOS NOMBRES Y APELLIDOS DEL EMPLEADO SON".NomApellido;
+echo "LOS NOMBRES Y APELLIDOS DEL EMPLEADO SON".NomApellidos;
 echo "EL CARGO DEL EMPLEADOES:  ".Cargo;
 echo "EL SALARIO DEL EMPLEADO ES :   ".Salario;
 ?>
